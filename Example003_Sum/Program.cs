72d0c8a54e35bbe4e0e6685971ec66f73604a129
Svetlana Popova <sspopova10-08@yandex.ru>
2022-03-03 06:43:17 +0300

    Added random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿//Вывести сумму двух целых чисел
-int numberA = 8;
-int numberB = 5;
+int numberA = new Random().Next(1, 9);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 9);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
