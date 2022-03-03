513241905c38f002caed651f143c0f195604ace3
Svetlana Popova <sspopova10-08@yandex.ru>
2022-03-03 07:28:42 +0300

    Added Example006_Task1

---
+++ Example006_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Поиск максимального из пяти чисел
+int a = 1;
+int b = 8;
+int c = 3;
+int d = 10;
+int e = 6;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine("max = " + max);
