4d3559f27fae52f1b6efb1611fd7826ce8aad29a
Svetlana Popova <sspopova10-08@yandex.ru>
2022-03-03 07:03:19 +0300

    Added Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿// Особенное приветствие для имени "Маша"
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
